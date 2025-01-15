bd1bec272bd12062f7c7d68c62bceb2b4a667d84
RedEnigmaZero <111486514+RedEnigmaZero@users.noreply.github.com>
2025-01-15 10:11:19 -0800

    2

--- src/main/java/com/ecs160/hw1/thread.java
+++ src/main/java/com/ecs160/hw1/thread.java
@@ -1,4 +1,9 @@
 package com.ecs160.hw1;
 
-public class thread {
+public class thread<T> {
+    protected ArrayList<T> post = new ArrayList<T>();
+
+    public thread(T obj) {
+        list.add(obj);
+    }
 }

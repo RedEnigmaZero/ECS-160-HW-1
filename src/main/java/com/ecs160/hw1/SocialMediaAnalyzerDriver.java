7c2a3ccae8c62445c6bbcea1373677e7c3fb9503
RedEnigmaZero <111486514+RedEnigmaZero@users.noreply.github.com>
2025-01-18 16:50:03 -0800

    o

--- src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
+++ src/main/java/com/ecs160/hw1/SocialMediaAnalyzerDriver.java
@@ -3,16 +3,43 @@ package com.ecs160.hw1;
 
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
+import com.google.gson.*;
 
 
 public class SocialMediaAnalyzerDriver {
+
+    private List<Post> parseJson(String filePath) throws FileNotFoundException {
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(new FileReader(filePath));
+        List<Post> posts = new ArrayList<>();
+
+        if (element.isJsonObject()) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            JsonArray feedArray = jsonObject.getAsJsonArray("feed");
+
+            for (JsonElement feedObject : feedArray) {
+                JsonObject thread = feedObject.getAsJsonObject().getAsJsonObject("thread");
+                posts.add(parseThread(thread));
+            }
+        }
+        return posts;
+    }
+
+    private Post parseThread(JsonObject thread) {
+
+    }
     public static void main(String[] args) throws IOException {
+        long x = 0;
         try {
+            // Get File (Not done) 
             File jsonInput = new File("input");
+            x = jsonInput.length();
             if (jsonInput.createNewFile()) {
                 System.out.println("File created: " + jsonInput.getName());
             } else {
@@ -21,6 +48,8 @@ public class SocialMediaAnalyzerDriver {
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
+        } finally {
+            System.out.println(x);
         }
     }
 }
--- src/test/java/com/ecs160/hw1/BasicAnalyzerTest.java
+++ src/test/java/com/ecs160/hw1/BasicAnalyzerTest.java
@@ -4,6 +4,7 @@ package com.ecs160.hw1;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class BasicAnalyzerTest {
 
